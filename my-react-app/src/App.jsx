782699c62691d2f7032ed4b3b510b24a0f400ab6
Evangelina <evangelinae2@nycstudents.net>
2026-05-03 19:34:55 +0000

    add image highlights and stuff

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.css';
+// Images: 
 import logo from './assets/not-av.jpg';
 import bio from './assets/logos/bio-tech.png';
 import future from './assets/logos/future-tech.png';
 import large from './assets/logos/large-tech.png';
 import sim from './assets/logos/simslogo.png';
-import freedom from './assets/logos/freedom-project.png';
+import freedom from './assets/logos/freedomlogo2.png';
+
 
 
 // function App() {
@@ -15,31 +18,27 @@ import freedom from './assets/logos/freedom-project.png';
 
 function HelloWorld() {
   return  <div className="container text-center"> 
+    <div>
+      <a href="./index.html"> <img src={freedom} alt="Freedom Project Logo" width="250px" /> </a>
+    </div>
     <div className="row m-md-2">
-      <div className="col-6">
-      <a href="./large-tech.html"> <img src={large} alt="Large Technology" width="350px" /> </a>
+      <div className="col-3">
+      <a href="./large-tech.html"> <img src={large} alt="Large Technology" width="250px" /> </a>
       </div>
-      <div className="col-6">
-      <a href="./future-tech.html"> <img src={future} alt="Future Technology" width="350px" /> </a>
+      
+      <div className="col-3">
+      <a href="./future-tech.html"> <img src={future} alt="Future Technology" width="250px" /> </a>
+      </div>
+       <div className="col-3">
+      <a href="./bio-tech.html"> <img src={bio} alt="Bio Technology" width="250px" /> </a>
+      </div>
+      <div className="col-3">
+      <a href="./sim-tech.html"> <img src={sim} alt="Simulation Technology" width="250px" /> </a>
+      </div>
       
     </div>
-    </div>
-    <div className="row text-center m-md-2">
-      <div className="col-8">
-      <img src={freedom} alt="Company Logo" width="400px" />
-      </div>
-    </div>
-    <div className="row d-flex justify-content-center m-md-2">
-      <div className="col-3">
-      <a href="./bio-tech.html"> <img src={bio} alt="Bio Technology" width="400px" /> </a>
-      </div>
-      <div className="col-3">
-      <a href="./sim-tech.html"> <img src={sim} alt="Simulation Technology" width="400px" /> </a>
-      </div>
-    </div>
+    </div>}
     
-   </div>;
-}
 
 const root = createRoot(document.getElementById('root'));
 root.render(<HelloWorld />);
